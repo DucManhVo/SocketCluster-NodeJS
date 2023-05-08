9d6621b1da39070e2a8442a86037ed4bbeea6008
DucManh <voducmanh000@gmail.com>
2023-05-08 11:02:54 +0700

    1st commit

---
+++ client1/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const socketClusterClient = require('socketcluster-client');
+const app = express()
+const port = 3000
+
+const socket = socketClusterClient.create({
+  hostname: 'localhost',
+  port: 8000
+});
+
+(async () => {
+  for await (let { error } of socket.listener('error')) {
+    console.error(error);
+  }
+})();
+
+(async () => {
+  for await (let event of socket.listener('connect')) {
+    console.log('Socket is connected');
+  }
+})();
+
+//Subscrise and watch mess
+(async () => {
+
+  // Subscribe to a channel.
+  let client1Channel = socket.subscribe('client1');
+
+  for await (let data of client1Channel) {
+    // ... Handle channel data.
+    console.log('Listen client1Channel: ' + data);
+  }
+
+  /*------ publish data to channel '2' ---------*/
+  let interval = setInterval(function () {
+    let date = new Date();
+    socket.transmitPublish('client2', 'client 1 send data test' + date.toISOString());
+  }, 6000);
+})();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ client2/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nodemon = require('nodemon');
+const socketClusterClient = require('socketcluster-client');
+const app = express()
+const port = 4000
+
+const socket = socketClusterClient.create({
+  hostname: 'localhost',
+  port: 8000
+});
+
+(async () => {
+  for await (let { error } of socket.listener('error')) {
+    console.error(error);
+  }
+})();
+
+(async () => {
+  for await (let event of socket.listener('connect')) {
+    console.log('Socket is connected');
+  }
+})();
+
+//Subscrise and watch mess
+(async () => {
+
+  // Subscribe to a channel.
+  let client2Channel = socket.subscribe('client2');
+
+  for await (let data of client2Channel) {
+    // ... Handle channel data.
+    console.log('Listen clientChannel: ' + data);
+  }
+
+  /*------ publish data to channel 'kai' ---------*/
+  let interval = setInterval(function () {
+    let date = new Date();
+    socket.transmitPublish('client1', 'client 2 send data test' + date.toISOString());
+  }, 5000);
+})();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
